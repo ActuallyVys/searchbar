9b78c6f9dc47709c3902506c7b0331f7f72c55b4
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-03-29 21:09:11 -0400

    Update suggestions.js

--- demo/js/suggestions.js
+++ demo/js/suggestions.js
@@ -26,3 +26,33 @@ let suggestions = [
     "What does HTML stand for?",
     "What does CSS stand for?",
 ];
+
+let timeout;
+
+document.querySelector('input').addEventListener('input', (event) => {
+  if (timeout) clearTimeout(timeout);
+  
+  timeout = setTimeout(async () => {
+
+    var url = 'https://duckduckgo.com/ac/?q=' + encodeURIComponent(event.target.value);
+
+    var Url = new URL(url);
+    
+    // uses bare to fetch because duckduckgo blocks CORS, can change to any proxy with UV for this
+    var fetched = await fetch('https://ludicrous.icu/bare/v1/', {
+      method: 'GET',
+      headers: {
+        'x-bare-headers': '{}',
+        'x-bare-protocol': 'https:',
+        'x-bare-host': 'duckduckgo.com',
+        'x-bare-port': 443,
+        'x-bare-forward-headers': '[]',
+        'x-bare-path': '/ac/?q=' + encodeURIComponent(event.target.value),
+      }
+    });
+    
+    (await fetched.json()).map(entry => {
+      suggestions.push(entry.phrase)
+    });
+  }, 200);
+}, false);
